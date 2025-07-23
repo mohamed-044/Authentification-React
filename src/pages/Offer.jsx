f951bcb54618b612f118ebdb0c17ac5c6ca39cf3
mohamed-044 <mohamed.boulatika04@gmail.com>
2025-07-23 11:50:20 +0200

    add authorization token

--- src/pages/Offer.jsx
+++ src/pages/Offer.jsx
@@ -16,7 +16,7 @@ const Offer = () => {
           {
             headers: {
               Accept: "application/json",
-              // Add Authorization token
+              Authorization: `Bearer ${JSON.parse(localStorage.getItem("auth"))?.token}`,
             },
           }
         );
