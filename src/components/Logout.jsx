0d8908302997d23014282f4fa563fdf6d4f8d5bf
mohamed-044 <mohamed.boulatika04@gmail.com>
2025-07-23 11:50:01 +0200

    remove token from local storage if user is disconnected and redirect on connexion page

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,20 +1,41 @@
 import { useEffect } from 'react';
-
+import { useNavigate } from "react-router";
 
 const Logout = () => {
+  const navigate = useNavigate();
    useEffect(() => {
     const handleLogout = async () => {
-      // (1) Appel API pour notifier la déconnexion
+      try {
+        const auth = JSON.parse(localStorage.getItem("auth"));
+        const token = auth?.token;
+        if (token) {
+        const response = await fetch('https://offers-api.digistos.com/api/logout', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`,
+          },
+        });
 
-      // (2) Suppression du token côté frontend
-
-      // (3) Redirection vers la page de login
+        if (!response.ok) {
+          const datas = await response.json();
+          throw new Error(`HTTP error: ${datas.message} (status: ${response.status})`);
+        }
+      } else {
+        console.error("Token manquant.");
+      }
+    } catch (error) {
+      console.error(error.message);
+    } finally {
+      localStorage.removeItem("auth");
+      navigate("/connexion");
+    }
     };
 
     handleLogout();
   }, []);
 
-  return null; // Pas besoin d'afficher quoi que ce soit
+  return null;
 };
 
 export default Logout;
