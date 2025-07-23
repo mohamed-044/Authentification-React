d54ca323bd14defe55a296f6438e3984d4a94254
mohamed-044 <mohamed.boulatika04@gmail.com>
2025-07-23 16:30:12 +0200

    add dispatch to logout user

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,20 +1,47 @@
-import { useEffect } from 'react';
-
+import { use, useEffect } from "react";
+import { useNavigate } from "react-router";
+import { useDispatch, useSelector } from "react-redux";
 
 const Logout = () => {
-   useEffect(() => {
+  const navigate = useNavigate();
+  const token = useSelector((state) => state.auth.token);
+  const dispatch = useDispatch();
+  useEffect(() => {
     const handleLogout = async () => {
-      // (1) Appel API pour notifier la déconnexion
+      try {
+        if (token) {
+          const response = await fetch(
+            "https://offers-api.digistos.com/api/logout",
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          );
 
-      // (2) Suppression du token côté frontend
-
-      // (3) Redirection vers la page de login
+          if (!response.ok) {
+            const datas = await response.json();
+            throw new Error(
+              `HTTP error: ${datas.message} (status: ${response.status})`
+            );
+          }
+        } else {
+          console.error("Token manquant.");
+        }
+      } catch (error) {
+        console.error(error.message);
+      } finally {
+        dispatch(Logout());
+        navigate("/connexion");
+      }
     };
 
     handleLogout();
   }, []);
 
-  return null; // Pas besoin d'afficher quoi que ce soit
+  return null;
 };
 
 export default Logout;
