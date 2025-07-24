9932c16c033170cf7ce7fe458126091e69aaf77a
mohamed-044 <mohamed.boulatika04@gmail.com>
2025-07-24 11:37:34 +0200

    url correction for logout

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -7,7 +7,7 @@ const Logout = () => {
     const handleLogout = async () => {
       try {
         const response = await fetch(
-          "https://offers-api.digistos.com/api/logout",
+          "https://offers-api.digistos.com/api/auth/logout",
           {
             method: "POST",
             headers: {
